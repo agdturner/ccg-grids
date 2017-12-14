238e80f0a9c118f63a58daa7481986bb0458b4b8
geoagdt <geoagdt@GEO-J20.ds.leeds.ac.uk>
2017-12-14 19:55:08 +0000

    Shortened HandleOutOfMemoryError to HOOME for brevity.

---
+++ src/test/java/uk/ac/leeds/ccg/andyt/grids/core/grid/chunk/Grids_GridChunkDoubleArrayTest.java
@@ -0,0 +1,186 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.leeds.ccg.andyt.grids.core.grid.chunk;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author geoagdt
+ */
+public class Grids_GridChunkDoubleArrayTest {
+    
+    public Grids_GridChunkDoubleArrayTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of initData method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testInitData() {
+        System.out.println("initData");
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        instance.initData();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getData method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testGetData() {
+        System.out.println("getData");
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        double[][] expResult = null;
+        double[][] result = instance.getData();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of clearData method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testClearData() {
+        System.out.println("clearData");
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        instance.clearData();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCell method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testGetCell() {
+        System.out.println("getCell");
+        int row = 0;
+        int col = 0;
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        double expResult = 0.0;
+        double result = instance.getCell(row, col);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of initCell method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testInitCell() {
+        System.out.println("initCell");
+        int row = 0;
+        int col = 0;
+        double valueToInitialise = 0.0;
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        instance.initCell(row, col, valueToInitialise);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCell method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testSetCell() {
+        System.out.println("setCell");
+        int row = 0;
+        int col = 0;
+        double valueToSet = 0.0;
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        double expResult = 0.0;
+        double result = instance.setCell(row, col, valueToSet);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of iterator method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testIterator() {
+        System.out.println("iterator");
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        Grids_GridChunkDoubleArrayOrMapIterator expResult = null;
+        Grids_GridChunkDoubleArrayOrMapIterator result = instance.iterator();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getArithmeticMeanDouble method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testGetArithmeticMeanDouble() {
+        System.out.println("getArithmeticMeanDouble");
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        double expResult = 0.0;
+        double result = instance.getArithmeticMeanDouble();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMin method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testGetMin() {
+        System.out.println("getMin");
+        boolean update = false;
+        boolean handleOutOfMemoryError = false;
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        Number expResult = null;
+        Number result = instance.getMin(update, handleOutOfMemoryError);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getMax method, of class Grids_GridChunkDoubleArray.
+     */
+    @Test
+    public void testGetMax() {
+        System.out.println("getMax");
+        boolean update = false;
+        boolean handleOutOfMemoryError = false;
+        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+        Number expResult = null;
+        Number result = instance.getMax(update, handleOutOfMemoryError);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
