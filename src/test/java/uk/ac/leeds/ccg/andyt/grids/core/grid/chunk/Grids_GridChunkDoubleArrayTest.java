51112af8788ab0cc424f0067f6d68827604e78ec
Andy Turner <a.g.d.turner@leeds.ac.uk>
2018-06-07 17:24:35 +0100

    Memory handling improvements for slope and aspect processing.

--- src/test/java/uk/ac/leeds/ccg/andyt/grids/core/grid/chunk/Grids_GridChunkDoubleArrayTest.java
+++ src/test/java/uk/ac/leeds/ccg/andyt/grids/core/grid/chunk/Grids_GridChunkDoubleArrayTest.java
@@ -37,150 +37,150 @@ public class Grids_GridChunkDoubleArrayTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of initData method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testInitData() {
-        System.out.println("initData");
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        instance.initData();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getData method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testGetData() {
-        System.out.println("getData");
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        double[][] expResult = null;
-        double[][] result = instance.getData();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of clearData method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testClearData() {
-        System.out.println("clearData");
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        instance.clearData();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getCell method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testGetCell() {
-        System.out.println("getCell");
-        int row = 0;
-        int col = 0;
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        double expResult = 0.0;
-        double result = instance.getCell(row, col);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of initCell method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testInitCell() {
-        System.out.println("initCell");
-        int row = 0;
-        int col = 0;
-        double valueToInitialise = 0.0;
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        instance.initCell(row, col, valueToInitialise);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setCell method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testSetCell() {
-        System.out.println("setCell");
-        int row = 0;
-        int col = 0;
-        double valueToSet = 0.0;
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        double expResult = 0.0;
-        double result = instance.setCell(row, col, valueToSet);
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of iterator method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testIterator() {
-        System.out.println("iterator");
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        Grids_GridChunkDoubleArrayOrMapIterator expResult = null;
-        Grids_GridChunkDoubleArrayOrMapIterator result = instance.iterator();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getArithmeticMeanDouble method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testGetArithmeticMeanDouble() {
-        System.out.println("getArithmeticMeanDouble");
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        double expResult = 0.0;
-        double result = instance.getArithmeticMeanDouble();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getMin method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testGetMin() {
-        System.out.println("getMin");
-        boolean update = false;
-        boolean handleOutOfMemoryError = false;
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        Number expResult = null;
-        Number result = instance.getMin(update);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getMax method, of class Grids_GridChunkDoubleArray.
-     */
-    @Test
-    public void testGetMax() {
-        System.out.println("getMax");
-        boolean update = false;
-        boolean handleOutOfMemoryError = false;
-        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
-        Number expResult = null;
-        Number result = instance.getMax(update);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    /**
+//     * Test of initData method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testInitData() {
+//        System.out.println("initData");
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        instance.initData();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getData method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testGetData() {
+//        System.out.println("getData");
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        double[][] expResult = null;
+//        double[][] result = instance.getData();
+//        assertArrayEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of clearData method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testClearData() {
+//        System.out.println("clearData");
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        instance.clearData();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getCell method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testGetCell() {
+//        System.out.println("getCell");
+//        int row = 0;
+//        int col = 0;
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        double expResult = 0.0;
+//        double result = instance.getCell(row, col);
+//        assertEquals(expResult, result, 0.0);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of initCell method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testInitCell() {
+//        System.out.println("initCell");
+//        int row = 0;
+//        int col = 0;
+//        double valueToInitialise = 0.0;
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        instance.initCell(row, col, valueToInitialise);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setCell method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testSetCell() {
+//        System.out.println("setCell");
+//        int row = 0;
+//        int col = 0;
+//        double valueToSet = 0.0;
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        double expResult = 0.0;
+//        double result = instance.setCell(row, col, valueToSet);
+//        assertEquals(expResult, result, 0.0);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of iterator method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testIterator() {
+//        System.out.println("iterator");
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        Grids_GridChunkDoubleArrayOrMapIterator expResult = null;
+//        Grids_GridChunkDoubleArrayOrMapIterator result = instance.iterator();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getArithmeticMeanDouble method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testGetArithmeticMeanDouble() {
+//        System.out.println("getArithmeticMeanDouble");
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        double expResult = 0.0;
+//        double result = instance.getArithmeticMeanDouble();
+//        assertEquals(expResult, result, 0.0);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getMin method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testGetMin() {
+//        System.out.println("getMin");
+//        boolean update = false;
+//        boolean handleOutOfMemoryError = false;
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        Number expResult = null;
+//        Number result = instance.getMin(update);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getMax method, of class Grids_GridChunkDoubleArray.
+//     */
+//    @Test
+//    public void testGetMax() {
+//        System.out.println("getMax");
+//        boolean update = false;
+//        boolean handleOutOfMemoryError = false;
+//        Grids_GridChunkDoubleArray instance = new Grids_GridChunkDoubleArray();
+//        Number expResult = null;
+//        Number result = instance.getMax(update);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
     
 }
